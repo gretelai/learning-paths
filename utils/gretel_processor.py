7d462502624021da85eb3b894147f0d3965a8716
Mason Egger <mason@masonegger.com>
2022-08-23 17:01:29 -0500

    first attempt auto push

--- utils/gretel_processor.py
+++ utils/gretel_processor.py
@@ -1,10 +1,15 @@
 import requests
 import os
-import json
+import sys
 import argparse
+from parser import parse_markdown
 
 bearer_token = os.getenv("BEARER_TOKEN", None)
 collection_id = os.getenv("COLLECTION_ID", None)
+
+if bearer_token is None or collection_id is None:
+    print("Bearer Token and Collection ID must be set via env vars")
+    sys.exit(1)
 
 headers = {
     "Accept-Version": "1.0.0",
@@ -26,9 +31,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Gretel Markdown to Webflow processor. Converts MD to HTML and uploads via the Webflow API"
     )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-c", "--create", action="store_true", help="Publish a new document"
+    )
+    group.add_argument(
+        "-u", "--update", action="store_true", help="Update an existing document"
+    )
     parser.add_argument(
         "-f", "--files", required=True, help="Comma separated list of files to process"
     )
+
     parser.add_argument(
         "-hh",
         "--headers",
@@ -38,15 +51,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for md_file in [x for x in args.files.split(",") if x[-3:] == ".md"]:
-        print(md_file)
+        metadata, html = parse_markdown(md_file)
 
-    # r = requests.post(
-    #    f"https://api.webflow.com/collections/{collection_id}/items",
-    #    headers=headers,
-    #    json=data,
-    # )
-    # print(r.request.url)
-    # print(r.request.body)
-    # print(r.request.headers)
-    # print(r.status_code)
-    # print(r.text)
+        data["name"] = metadata["title"]
+        data["slug"] = metadata["slug"]
+        data["document"] = html
+
+        r = requests.post(
+            f"https://api.webflow.com/collections/{collection_id}/items",
+            headers=headers,
+            json=data,
+        )
+        # print(r.request.url)
+        # print(r.request.body)
+        # print(r.request.headers)
+        print(r.status_code)
+        print(r.text)
