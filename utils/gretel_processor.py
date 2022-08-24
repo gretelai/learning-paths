1593e7b520d1d56d44d70e677bc5460942c1d4c1
Mason Egger <mason@masonegger.com>
2022-08-24 15:50:09 -0500

    tweaks

--- utils/gretel_processor.py
+++ utils/gretel_processor.py
@@ -2,13 +2,14 @@ import requests
 import os
 import sys
 import argparse
+import logging
 from parser import parse_markdown
 
 bearer_token = os.getenv("BEARER_TOKEN", None)
 collection_id = os.getenv("COLLECTION_ID", None)
 
 if bearer_token in (None, "") or collection_id in (None, ""):
-    print("Bearer Token and Collection ID must be set via env vars")
+    logging.error("Bearer Token and Collection ID must be set via env vars")
     sys.exit(1)
 
 headers = {
@@ -41,7 +42,7 @@ def get_ids_from_slugs(collection):
     return ids
 
 
-def create_learning_path(files):
+def create_learning_paths(files):
     for md_file in files:
         metadata, html = parse_markdown(md_file)
 
@@ -49,28 +50,30 @@ def create_learning_path(files):
         data["fields"]["slug"] = metadata["slug"]
         data["fields"]["document"] = html
 
+        logging.info(f"Creating {md_file}")
         r = requests.post(
             f"https://api.webflow.com/collections/{collection_id}/items",
             headers=headers,
             json=data,
         )
-        # print(r.request.url)
-        # print(r.request.body)
-        # print(r.request.headers)
-        print(r.status_code)
+
+        logging.info(f"{md_file} creation returned with status code {r.status_code}")
 
 
-def update_learning_path(files, slugs, force=False):
+def update_learning_paths(files, slugs, force=False):
     for md_file in files:
         metadata, html = parse_markdown(md_file)
 
         if metadata["slug"] not in slugs:
             if force is True:
-                create_learning_path([md_file])
+                logging.info(
+                    f"Updated file not found in webflow. Sending {md_file} to create function"
+                )
+                id = create_learning_paths([md_file])
                 continue
             else:
-                print(
-                    "WARNING: File to update doesn't exist in Webflow. To force a create use the --force option"
+                logging.warning(
+                    "File to update doesn't exist in Webflow. To force a create use the --force option"
                 )
                 continue
 
@@ -79,18 +82,18 @@ def update_learning_path(files, slugs, force=False):
         data["fields"]["document"] = html
 
         id = slugs[metadata["slug"]]
+
+        logging.info(f"Updating {md_file}")
         r = requests.put(
             f"https://api.webflow.com/collections/{collection_id}/items/{id}",
             headers=headers,
             json=data,
         )
-        # print(r.request.url)
-        # print(r.request.body)
-        # print(r.request.headers)
-        print(r.status_code)
+
+        logging.info(f"{md_file} update returned with status code {r.status_code}")
 
 
-def delete_learning_path(files, slugs):
+def delete_learning_paths(files, slugs):
     for md_file in files:
         file_name = os.path.basename(md_file[:-3])
         if file_name not in slugs:
@@ -101,38 +104,57 @@ def delete_learning_path(files, slugs):
 
         id = slugs[file_name]
 
+        logging.info(f"Deleting {md_file}")
         r = requests.delete(
             f"https://api.webflow.com/collections/{collection_id}/items/{id}",
             headers=headers,
         )
 
-        print(f"{md_file} DELETED with code {r.status_code}")
+        logging.info(f"{md_file} delete returned with status code {r.status_code}")
+
+
+def publish_learning_paths(ids):
+    r = requests.put(
+        f"https://api.webflow.com/collections/{collection_id}/items/publish",
+        headers=headers,
+        json={"itemIds": ids},
+    )
+    logging.info(
+        f"The files with the following ids: {ids} published and returned with status code {r.status_code}"
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Gretel Markdown to Webflow processor. Converts MD to HTML and uploads via the Webflow API"
     )
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument(
-        "-c", "--create", action="store_true", help="Publish a new document"
+    actions = parser.add_mutually_exclusive_group(required=True)
+    actions.add_argument(
+        "-c", "--create", action="store_true", help="Publish new Learning Paths"
     )
-    group.add_argument(
-        "-u", "--update", action="store_true", help="Update an existing document"
+    actions.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="Update specified existing Learning Paths",
     )
-    group.add_argument(
-        "-d", "--delete", action="store_true", help="Delete an existing document"
+    actions.add_argument(
+        "-d",
+        "--delete",
+        action="store_true",
+        help="Delete specified existing Learning Paths",
     )
+
     parser.add_argument(
-        "-f", "--files", required=True, help="Comma separated list of files to process"
+        "-f", "--files", help="Comma separated list of files to process"
     )
+
     parser.add_argument(
         "-fo",
         "--force",
         action="store_true",
         help="Force create documents on update if they don't exist",
     )
-
     parser.add_argument(
         "-hh",
         "--headers",
@@ -141,13 +163,14 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    files = [x for x in args.files.split(",") if x[-3:] == ".md"]
+    if args.files is not None:
+        files = [x for x in args.files.split(",") if x[-3:] == ".md"]
 
     slugs = get_ids_from_slugs(collection_id)
 
     if args.create is True:
-        create_learning_path(files)
+        create_learning_paths(files)
     elif args.update is True:
-        update_learning_path(files, slugs, force=args.force)
+        update_learning_paths(files, slugs, force=args.force)
     elif args.delete is True:
-        delete_learning_path(files, slugs)
+        delete_learning_paths(files, slugs)
